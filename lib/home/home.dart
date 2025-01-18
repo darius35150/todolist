a0311f3f738883ee9edcb443dd5e08f4401604c6
darius35150 <darius35150@yahoo.com>
2025-01-17 22:14:46 -0600

    fixed list scrolling to adjust to screen size.

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:todolist/db/rowitemslist.dart';
 import 'package:flutter_bootstrap/flutter_bootstrap.dart';
@@ -19,8 +21,12 @@ class _Home extends State<Home> {
     const Color.fromARGB(255, 89, 235, 179)
   ];
 
+  double height =
+      window.physicalSize.height + (RowItemsList.getAllItems().length * 30);
+
   Future<void> _saveData() async {
     setState(() {
+
       if (textFormFieldController.text.isEmpty) {
         showDialog<void>(
           context: context,
@@ -51,6 +57,8 @@ class _Home extends State<Home> {
         RowItemsList itemsList =
             RowItemsList(textFormFieldController.text, DateTime.now());
         itemsList.saveItems();
+              height =
+          window.physicalSize.height + (RowItemsList.getAllItems().length * 30);
         textFormFieldController.clear();
       }
     });
@@ -60,62 +68,65 @@ class _Home extends State<Home> {
     setState(() {
       RowItemsList.deleteItem(
           RowItemsList.getAllItems().elementAt(index)["itemName"]);
+      height =
+          window.physicalSize.height + (RowItemsList.getAllItems().length * 30);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 89, 235, 179),
-          title: const Text("To Do List",
-            style: TextStyle(
-              color: Colors.white
-            ),),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(255, 89, 235, 179),
+        title: const Text(
+          "To Do List",
+          style: TextStyle(color: Colors.white),
         ),
-        body: BootstrapContainer(fluid: false, children: [
-          BootstrapRow(
-            children: [
-            BootstrapCol(
+      ),
+      body: BootstrapContainer(fluid: false, children: [
+        BootstrapRow(children: [
+          BootstrapCol(
               sizes: "col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3",
-                fit: FlexFit.tight,
-                child: Theme(
-                    data: Theme.of(context).copyWith(
-                        colorScheme:
-                            const ColorScheme.light(primary: Color.fromARGB(255, 89, 235, 179))),
-                    child: TextField(
-                      controller: textFormFieldController,
-                      style: const TextStyle(fontFamily: "Nunito"),
-                      cursorColor:  const Color.fromARGB(255, 89, 235, 179),
-                      onSubmitted: (value) =>{
-                           _saveData()
-                      },
-                      decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: "Enter item name"),
-                    ))),
-          ]),
-          BootstrapRow(children: [
-            BootstrapCol(
+              fit: FlexFit.tight,
+              child: Theme(
+                  data: Theme.of(context).copyWith(
+                      colorScheme: const ColorScheme.light(
+                          primary: Color.fromARGB(255, 89, 235, 179))),
+                  child: TextField(
+                    controller: textFormFieldController,
+                    style: const TextStyle(fontFamily: "Nunito"),
+                    cursorColor: const Color.fromARGB(255, 89, 235, 179),
+                    onSubmitted: (value) => {_saveData()},
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: "Enter item name"),
+                  ))),
+        ]),
+        BootstrapRow(children: [
+          BootstrapCol(
               sizes: "col-5 col-sm-3 col-md-2 col-lg-2 col-xl-2",
               fit: FlexFit.tight,
-                child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 15, backgroundColor: const Color.fromARGB(255, 89, 235, 179)),
-                        onPressed: _saveData,
-                        child: const Text("Save",
-                          style: TextStyle(
-                            color: Colors.white
-                          ),))))
-          ]),
-          BootstrapRow(
-            children: [  BootstrapCol(
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          elevation: 15,
+                          backgroundColor:
+                              const Color.fromARGB(255, 89, 235, 179)),
+                      onPressed: _saveData,
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(color: Colors.white),
+                      ))))
+        ]),
+        const SizedBox(height: 50),
+        BootstrapRow(children: [
+          BootstrapCol(
               child: Center(
                   child: Container(
                       width: 600,
-                      height: 600,
+                      height: height,
                       margin: const EdgeInsets.fromLTRB(0, 25, 0, 0),
                       child: ListView.builder(
                           scrollDirection: Axis.vertical,
@@ -146,7 +157,9 @@ class _Home extends State<Home> {
                                     ],
                                   ),
                                 ));
-                          }))))])
-        ]));
+                          }))))
+        ])
+      ]),
+    );
   }
 }
